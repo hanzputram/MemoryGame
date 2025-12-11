acea73e59b85d1dc7bbb15490d5c700a74622c12
hanzputram <codmm7083@gmail.com>
2025-12-11 11:52:26 +0700

    Fix fetch finish score + update DB config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+fetch('/memory/finish', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+    },
+    body: JSON.stringify(payload),
+})
